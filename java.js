de0da40bf5f8e5e99f2c85dafff366cedfde4d68
Portia Greene <pd12300@yahoo.com>
2021-10-20 23:51:46 -0400

    Spreaddestructduncexercise homework completed

---
+++ java.js
@@ -0,0 +1,26 @@
+const jpDinos = [
+    'Velociraptors'
+    'Tyrannosaurus Rex',
+     'Dilophosaurus'
+];
+
+function seeDinos( a, ...b){
+console.log(a);
+console.log(b);
+}
+ seeDinos(...jpDinos);
+
+
+
+const jpCharacters = { 
+
+   alanGrant: 'Sam Neill',
+   ellieSattler: 'Laura Dern',
+   ianMalcolm: 'Jeff Goldblum'
+};
+
+seeCharacters = ({ellieSattler}) =>{
+    console.log(ellieSattle);
+};
+
+console.log(jPCharacters);
